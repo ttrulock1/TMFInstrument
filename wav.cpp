33f6352b4501a89282409a7412e9ba3487e0dd8a
Todd Trulock <toddstrulock@gmail.com>
2024-11-18 15:55:09 -0700

    updated wav.cpp file using tech. will look back on this.

--- wav.cpp
+++ wav.cpp
@@ -1,85 +1,88 @@
-// this structure is the minimal required header data for a wav file.
+#include <cstdio>
+#include <cstring>
+#include <cstdint>
+
 struct SMinimalWaveFileHeader
 {
-    //the main chunk
+    // main chunk
     unsigned char m_szChunkID[4];
-    uint32 m_nChunkSize;
+    uint32_t m_nChunkSize;
     unsigned char m_szFormat[4];
 
-    //sub chunk 1 "fmt "
+    // subchunk 1 "fmt "
     unsigned char m_szSubChunk1ID[4];
-    uint32 m_nSubChunkSize;
-    uint16 m_nAudioFormat;
-    uint16 m_nNumChannels;
-    uint32 m_nSampleRate;
-    uint32 m_nByteRate;
-    uint16 m_nBlockAlign;
-    uint16 m_nBitsPersample;
+    uint32_t m_nSubChunkSize;
+    uint16_t m_nAudioFormat;
+    uint16_t m_nNumChannels;
+    uint32_t m_nSampleRate;
+    uint32_t m_nByteRate;
+    uint16_t m_nBlockAlign;
+    uint16_t m_nBitsPerSample;
 
-    //sub chunk 2 "data"
+    // subchunk 2 "data"
     unsigned char m_szSubChunk2ID[4];
-
-    uint32 m_nChunkSize;
-    unsigned char m_szFormat[4];
-
-    //sub chunk 1 "fmt "
-    unsigned char m_szSubChunk1ID[4];
-    uint32 m_nSubChunkSize;
-    uint16 m_nAudioFormat;
-    uint16 m_nNumChannels;
-    uint32 m_nSampleRate;
-    uint32 m_nByteRate;
-    uint16 m_nBlockAlign;
-    uint16 m_nBitsPersample;
+    uint32_t m_nSubChunk2Size;
 };
 
-bool WriteWaveFile(const char *szFileName, void *pData, int32 nDataSize, int16 nNumChannels, int32 nSampleRate, int32 nBitsPersample)
+bool WriteWaveFile(const char *szFileName, void *pData, int32_t nDataSize, int16_t nNumChannels, int32_t nSampleRate, int32_t nBitsPerSample)
 {
-    // Open the file if we can
+    // Open the file
     FILE *File = fopen(szFileName, "w+b");
-    if(!File){
+    if (!File) {
         return false;
     }
+
     SMinimalWaveFileHeader waveHeader;
-    //fill out the main chunk
-    memcpy(waveHeader.m_szChunkID,"Riff", 4);
+    // Fill out the main chunk
+    memcpy(waveHeader.m_szChunkID, "RIFF", 4);
     waveHeader.m_nChunkSize = nDataSize + 36;
-    memcpy(waveHeader.m_szFormat, "Wave", 4);
+    memcpy(waveHeader.m_szFormat, "WAVE", 4);
 
-    // fill out sub chunk 1 "fmt"
-    memcpy(waveHeader.m_szSubChunk1ID, "FMT", 4);
-    waveHeader.m_nSubChunkSize= 16;
-    waveHeader.m_nAudioFormat=1;
-    waveHeader.m_nNumChannels= nNumChannels;
-    waveHeader.m_nSampleRate= nSampleRate;
-    waveHeader.m_nByteRate = nSampleRate * nNumChannels * nBitsPersample / 8;
+    // Fill out subchunk 1 "fmt "
+    memcpy(waveHeader.m_szSubChunk1ID, "fmt ", 4);
+    waveHeader.m_nSubChunkSize = 16;
+    waveHeader.m_nAudioFormat = 1; // PCM format
+    waveHeader.m_nNumChannels = nNumChannels;
+    waveHeader.m_nSampleRate = nSampleRate;
+    waveHeader.m_nByteRate = nSampleRate * nNumChannels * nBitsPerSample / 8;
+    waveHeader.m_nBlockAlign = nNumChannels * nBitsPerSample / 8;
+    waveHeader.m_nBitsPerSample = nBitsPerSample;
 
-    //fill out sub chunk 2 "data"
-    memcpy(wave.m_szSubChunk1ID, "data", 4);
-    waveHeader.m_nSubChunkSize = nDataSize;
+    // Fill out subchunk 2 "data"
+    memcpy(waveHeader.m_szSubChunk2ID, "data", 4);
+    waveHeader.m_nSubChunk2Size = nDataSize;
 
-    //write the header
-    fwrite(&waveHeader, sizeof(SMinimalWaveFileHeader),1,File );
+    // Write the header
+    fwrite(&waveHeader, sizeof(SMinimalWaveFileHeader), 1, File);
 
-    //write the wave data itself
+    // Write the wave data itself
     fwrite(pData, nDataSize, 1, File);
 
-    // close the file and return success
+    // Close the file and return success
     fclose(File);
     return true;
 }
 
-int nSampleRate = 44100;
-int nNumSeconds = 4;
-int nNumChannels = 1;
+// Example usage
+int main() {
+    int32_t nSampleRate = 44100;
+    int nNumSeconds = 4;
+    int16_t nNumChannels = 1;
+    int nBitsPerSample = 16;
 
-int nNumSamples = nSampleRate * nNumChannels * nNumSeconds;
-int32 *pData = new int32[nNumSamples];
+    int nNumSamples = nSampleRate * nNumChannels * nNumSeconds;
+    int16_t *pData = new int16_t[nNumSamples];
 
-int32 nValue = 0;
-for(int nIndex = 0; nIndex < nNumSamples; ++nIndex){
-    nValue += 8000000;
-    pData[nIndex] = nValue;
+    // Generate a simple waveform (sawtooth wave as an example)
+    int16_t nValue = 0;
+    for (int nIndex = 0; nIndex < nNumSamples; ++nIndex) {
+        nValue = (nValue + 8000) % 32767; // Increment to create a basic wave pattern
+        pData[nIndex] = nValue;
+    }
+
+    // Write to a WAV file
+    WriteWaveFile("outmono.wav", pData, nNumSamples * sizeof(int16_t), nNumChannels, nSampleRate, nBitsPerSample);
+
+    delete[] pData;
+    return 0;
 }
-
-WriteWaveFile("outmono.wav", pData, nNumberSamples * sizeof(pData[0]), nNumChannels, nSampleRate, sizeof(pData[0])*8);
